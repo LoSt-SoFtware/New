10b3d63c2f806609c4115803120cee284c9aa53b
Aro Software <75952429+LoSt-SoFtware@users.noreply.github.com>
2021-02-23 13:56:57 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,70 @@
+import  os, sys , time
+os.system("clear")
+time.sleep(2)
+
+lost = """\033[97m
+
+ .o88b.  .d8b.  d888888b 
+d8P  Y8 d8' `8b `~~88~~' 
+8P      88ooo88    88    
+8b      88~~~88    88    
+Y8b  d8 88   88    88    
+ `Y88P' YP   YP    YP    
+                         
+---------------------------------------------------------
+
+Auther : LOST
+
+Github : github.com/LoSt-SoFtware
+
+t.me    :  @LO1STT
+
+YT       :  LOST KURDISH
+
+---------------------------------------------------------"""
+while True:
+	print (lost) 
+	time.sleep(2)
+	
+	h = "cat"
+	j = "lost"
+	jj = input("  Username: ")
+	hh = input("  Passowrd: ")
+	print ('loading----------------------')
+	time.sleep(2)
+	if h == hh and j == jj :
+		print("\033[32m Afarin \033[0;1m:)")
+		os.system ('python2 ca')
+		time.sleep(2)
+		print("loading-----------------------\033[97m")
+		print(50*"-")
+		
+		time.sleep(1)
+		time.sleep(2)
+		os.system("pyth")
+	else:
+		print("\033[92mHallaya !!")
+		time.sleep(1)
+		os.system("clear")
+### lost ###
+
+lost = """\033[97m
+
+ .o88b.  .d8b.  d888888b 
+d8P  Y8 d8' `8b `~~88~~' 
+8P      88ooo88    88    
+8b      88~~~88    88    
+Y8b  d8 88   88    88    
+ `Y88P' YP   YP    YP    
+                         
+---------------------------------------------------------
+
+Auther : LOST
+
+Github : github.com/LoSt-SoFtware
+
+t.me     :  @LO1STT
+
+YT        :  LOST KURDISH
+
+-----------------------------------------------------------"""
